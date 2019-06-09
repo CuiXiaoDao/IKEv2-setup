e9c0029b33596883c4efc2f9329b6a7e9b3f1803
CuiXiaoDao <cuichao@zju.edu.cn>
2019-06-09 12:01:17 +0800

    remove iptables drop

--- setup.sh
+++ setup.sh
@@ -183,10 +183,10 @@ iptables -t nat -A POSTROUTING -s $VPNIPPOOL -o $ETH0ORSIMILAR -m policy --pol i
 iptables -t nat -A POSTROUTING -s $VPNIPPOOL -o $ETH0ORSIMILAR -j MASQUERADE
 
 
-# fall through to drop any other input and forward traffic
+# # fall through to drop any other input and forward traffic
 
-iptables -A INPUT   -j DROP
-iptables -A FORWARD -j DROP
+# iptables -A INPUT   -j DROP
+# iptables -A FORWARD -j DROP
 
 iptables -L
 
